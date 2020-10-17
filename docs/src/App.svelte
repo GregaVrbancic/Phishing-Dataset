af72e96a02215206480cd8555a7077dab92a1a78
Grega Vrbančič <grega.vrbancic@gmail.com>
2020-10-17 14:29:52 +0200

    Replace dummy text

--- docs/src/App.svelte
+++ docs/src/App.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Phishing Datasets Web App</h1>
+	<p>Under development!</p>
 </main>
 
 <style>
