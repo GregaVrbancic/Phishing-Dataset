c6bc98df35f9d59c43b2509364b5598d4576f537
Grega Vrbančič <grega.vrbancic@gmail.com>
2020-10-21 11:07:27 +0200

    WIP

--- web-app/src/App.svelte
+++ web-app/src/App.svelte
@@ -1,29 +1,58 @@
 <script>
+  import NavBar from './NavBar.svelte';
 </script>
 
-<main>
-	<h1>Phishing Datasets Web App</h1>
-	<p>Under the development!</p>
-</main>
+<style type="text/scss">
+@import "bulma/bulma";
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  :global(a) {
+    color: #ff3e00;
+    &:hover {
+      text-decoration: none;
+    }
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  .title {
+    font-size: 1rem;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .subtitle {
+    font-size: 1rem;
+    font-style: italic;
+  }
 </style>
+
+<main>
+
+  <NavBar />
+
+  <section class="section">
+	<div class="columns is-desktop">
+		<div class="column is-three-quarters">
+		  <div class="columns">
+			<div class="column is-one-third">
+				<p class="title">Configure own dataset variation</p>
+				<p class="subtitle">Select the dataset ratio and features.</p>
+			</div>
+			<div class="column"><p class="title">Dataset variation preview</p></div>
+		  </div>
+		</div>
+		<div class="column">
+			<p class="title">Download the original dataset variations</p>
+		</div>
+	  </div>
+  </section>
+
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Phishing Dataset Web App</strong>
+        by
+        <a href="https://grega.xyz">Grega Vrbančič</a>
+        . The source code is licensed
+        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>
+        . The website content.
+      </p>
+    </div>
+  </footer>
+</main>
---
+++ web-app/src/NavBar.svelte
@@ -0,0 +1,53 @@
+<script>
+  import { GithubIcon } from 'svelte-feather-icons'
+</script>
+
+<style type="text/scss">
+  .navbar {
+    border-bottom: 1px solid #efefef;
+
+    .navbar-brand {
+      display: flex;
+      flex-grow: 1;
+      justify-content: center;
+
+      .navbar-item {
+        h1 {
+            color: #ff3e00;
+            text-transform: uppercase;
+            font-size: x-large;
+            font-weight: 300;
+            line-height: 3rem;
+        }
+      }
+    }
+
+    .navbar-menu {
+      flex-grow: 0;
+    }
+  }
+
+
+
+
+</style>
+
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="/">
+      <h1>Phishing Datasets Web App</h1>
+    </a>
+  </div>
+
+  <div id="navbar" class="navbar-menu">
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a>
+            <GithubIcon size="24" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
